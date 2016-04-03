f200cba8d58081f134eec487ea09ab39485713dc
Mike <mikestiver@hotmail.com>
2016-04-03 17:17:25 -0400

    Solution to the Slow Typest problem in F#

---
+++ Program.fs
@@ -0,0 +1,22 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+let calcDistance (input:string) =
+    input.ToCharArray()
+        |> Array.toSeq 
+        |> Seq.pairwise
+        |> Seq.map(fun x -> ("123456789.0".IndexOf(snd x), "123456789.0".IndexOf(fst x)))
+        |> Seq.sumBy (fun x-> System.Math.Sqrt(System.Math.Pow(float(fst (x) / 3 - snd (x) / 3), 2.0) + System.Math.Pow(float(fst (x) % 3 - snd (x) % 3), 2.0)))
+                
+[<EntryPoint>]
+let main argv = 
+    let testData = ["7851"; "219.45.143.143"]        
+
+    for i in testData do
+        printfn "%s -> %A" i (calcDistance i)
+
+    //List.zip input.Tail
+    //    |> printfn "%A "
+    
+    System.Console.ReadKey() |> ignore
+    
+    0 // return an integer exit code
